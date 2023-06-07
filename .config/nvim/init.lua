5de6999b9e4d29e087e95d7b715be67062a75275
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2023-06-07 17:57:57 -0600

    adds from scratch config for neovim to accomodate vscode-neovim

---
+++ .config/nvim/init.lua
@@ -0,0 +1,58 @@
+vim.g.mapleader = [[ ]]
+vim.g.maplocalleader = [[,]]
+
+
+local lazypath = vim.fn.stdpath("data") .. "/nvim-vscode/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--single-branch",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+
+vim.opt.runtimepath:prepend(lazypath)
+
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
+require("lazy").setup("plugins", {
+  defaults = { lazy = true },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        "gzip",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
+
+vim.api.nvim_set_keymap("x", "gc", "<Plug>VSCodeCommentary", {})
+vim.api.nvim_set_keymap("n", "gc", "<Plug>VSCodeCommentary", {})
+vim.api.nvim_set_keymap("o", "gc", "<Plug>VSCodeCommentary", {})
+vim.api.nvim_set_keymap("n", "gcc", "<Plug>VSCodeCommentaryLine", {})
+
+require("impatient")
+require("config.options")
+require("config.keymaps")
+
+vim.cmd [[
+    highlight OperatorSandwichBuns guifg='#aa91a0' gui=underline ctermfg=172 cterm=underline
+    highlight OperatorSandwichChange guifg='#edc41f' gui=underline ctermfg='yellow' cterm=underline
+    highlight OperatorSandwichAdd guibg='#b1fa87' gui=none ctermbg='green' cterm=none
+    highlight OperatorSandwichDelete guibg='#cf5963' gui=none ctermbg='red' cterm=none
+]]
+
+vim.cmd.colorscheme "catppuccin"
+
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,38 @@
+return {
+  "lewis6991/impatient.nvim",
+  { "catppuccin/nvim", name = "catppuccin" },
+  {
+    "ggandor/leap.nvim",
+    event = "VeryLazy",
+    dependencies = "tpope/vim-repeat",
+    config = function()
+      local map = vim.api.nvim_set_keymap
+      -- 2-character Sneak (default)
+      local opts = { noremap = false }
+      map("n", "z", "<Plug>(leap-forward-x)", opts)
+      map("n", "Z", "<Plug>(leap-backward-x)", opts)
+
+      -- visual-mode
+      map("x", "z", "<Plug>(leap-forward-x)", opts)
+      map("x", "Z", "<Plug>(leap-backward-x)", opts)
+
+      -- operator-pending-mode
+      map("o", "z", "<Plug>(leap-forward-x)", opts)
+      map("o", "Z", "<Plug>(leap-backward-x)", opts)
+    end,
+  },
+  {
+    "ggandor/flit.nvim",
+    opts = { labeled_modes = "nv" },
+    event = "VeryLazy",
+  },
+  { 'machakann/vim-sandwich', event = 'VeryLazy', enabled = true },
+  {
+    "echasnovski/mini.surround",
+    event = "VeryLazy",
+    config = function()
+      require("mini.surround").setup {}
+    end,
+    enabled = false
+  },
+}
