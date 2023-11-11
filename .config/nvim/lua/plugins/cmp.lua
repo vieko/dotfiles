8e29f4729dfca6e30c56ac21d6b21231830b1df9
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2023-11-11 16:16:49 -0700

    saves tweaks and keymaps for completion

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -22,21 +22,43 @@ return {
 
       cmp.setup({
         formatting = lsp_zero.cmp_format(),
-        mapping = cmp.mapping.preset.insert({
-          -- `Enter` key to confirm completion
-          ['<CR>'] = cmp.mapping.confirm({ select = false }),
-
-          -- Ctrl+Space to trigger completion menu
-          ['<C-Space>'] = cmp.mapping.complete(),
-
-          -- Scroll up and down in the completion documentation
-          ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-d>'] = cmp.mapping.scroll_docs(4),
-
-          -- Navigate between snippet placeholder
-          ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-          ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-        })
+        mapping = {
+          ['<C-y>'] = cmp.mapping.confirm({ select = false }),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<Up>'] = cmp.mapping.select_prev_item({ behavior = 'select' }),
+          ['<Down>'] = cmp.mapping.select_next_item({ behavior = 'select' }),
+          ['<C-p>'] = cmp.mapping(function()
+            if cmp.visible() then
+              cmp.select_prev_item({ behavior = 'insert' })
+            else
+              cmp.complete()
+            end
+          end),
+          ['<C-n>'] = cmp.mapping(function()
+            if cmp.visible() then
+              cmp.select_next_item({ behavior = 'insert' })
+            else
+              cmp.complete()
+            end
+          end),
+        },
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
+        -- mapping = cmp.mapping.preset.insert({
+        -- `Enter` key to confirm completion
+        -- ['<CR>'] = cmp.mapping.confirm({ select = false }),
+        -- Ctrl+Space to trigger completion menu
+        -- ['<C-Space>'] = cmp.mapping.complete(),
+        -- Scroll up and down in the completion documentation
+        -- ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        -- ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        -- Navigate between snippet placeholder
+        -- ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        -- ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+        -- })
       })
     end
   },
