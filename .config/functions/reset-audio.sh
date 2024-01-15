0cc23fcf0aed8dc58b202a734c171e6c14c0e84f
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2024-01-15 14:12:11 -0700

    removes redundant function by consolidating sound and easyeffect

--- .config/functions/reset-audio.sh
+++ .config/functions/reset-audio.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
+# restart wireplumber, pipewire and pulse
 systemctl --user restart wireplumber pipewire pipewire-pulse
+
+# reset pulseaudio confi
 rm -r ~/.config/pulse
+
+# restart easyeffects
+flatpak kill com.github.wwmm.easyeffects
+
+# wait for easyeffects to close
+sleep 1
+
+# start easyeffects in the background
+flatpak run com.github.wwmm.easyeffects &
+
+# wait for easyeffects to start
+sleep 2
+
+# run easyeffects command to set the correct output
+pw-metadata -n settings 0 clock.force-quantum 2048
