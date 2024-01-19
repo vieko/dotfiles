1f0d2b48297f4b1dec27e98d41f3886358880a6f
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2024-01-19 08:54:02 -0700

    adds missing emmet config

---
+++ .config/nvim/lua/vieko/plugins/configs/emmet.lua
@@ -0,0 +1,14 @@
+vim.g.user_emmet_settings = {
+  typescriptreact = {
+    extends = "jsx"
+  },
+  typescript = {
+    extends = "jsx"
+  },
+  ['typescript.tsx'] = {
+    extends = "jsx"
+  },
+  ['javascript.jsx'] = {
+    extends = "jsx"
+  },
+}
