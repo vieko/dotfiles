19dc1a4081bac20a786a04853ddee25f1a95abbb
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2024-01-24 13:23:59 -0700

    improves close commands and prevents neotree from shutting down nvim

--- .config/nvim/lua/vieko/plugins/configs/neo-tree.lua
+++ .config/nvim/lua/vieko/plugins/configs/neo-tree.lua
@@ -7,7 +7,7 @@ require("neo-tree").setup({
     "git_status",
     "document_symbols",
   },
-  close_if_last_window = true,
+  close_if_last_window = false,
   default_component_configs = {
     diagnostics = {
       symbols = {
