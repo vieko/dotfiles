b447046c1183f7608f273d9cab25012adbe19ce1
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2024-04-10 17:36:43 -0600

    updates toggle theme function to handle fzf

--- .config/functions/toggle-theme.sh
+++ .config/functions/toggle-theme.sh
@@ -19,6 +19,10 @@ if [ "$CURRENT_MODE" = "'prefer-dark'" ]; then
   gsettings set org.gnome.desktop.interface gtk-theme 'Pop'
   sed -i "s/$DARKTHEME/$LIGHTTHEME/" "$TMUXCONF" "$KITTYCONF"
   sed -i 's/dark/light/' "$VIMCONF"
+  fish -c 'set -Ux FZF_DEFAULT_OPTS "\
+  --color=bg+:#ccd0da,bg:#eff1f5,spinner:#dc8a78,hl:#d20f39 \
+  --color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \
+  --color=marker:#dc8a78,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39"'
   if [ -n "$TMUX" ]; then
     switch_vim_theme_inside_tmux "light"
   fi
@@ -28,6 +32,10 @@ else
   gsettings set org.gnome.desktop.interface gtk-theme 'Pop-dark'
   sed -i "s/$LIGHTTHEME/$DARKTHEME/" "$TMUXCONF" "$KITTYCONF"
   sed -i 's/light/dark/' "$VIMCONF"
+  fish -c 'set -Ux FZF_DEFAULT_OPTS "\
+  --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+  --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+  --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"'
   if [ -n "$TMUX" ]; then
     switch_vim_theme_inside_tmux "dark"
   fi
