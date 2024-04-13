f726390c869e580be034bd680aa2d4e7515bdd41
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2024-04-13 14:55:56 -0600

    updates colors for vim mode on fish

--- .config/fish/functions/fish_mode_prompt.fish
+++ .config/fish/functions/fish_mode_prompt.fish
@@ -1,19 +1,19 @@
 function fish_mode_prompt
   switch $fish_bind_mode
     case default
-      set_color --bold $fish_color_command
+      set_color --bold $fish_color_gray
       echo 'N '
     case insert
-      set_color --bold $fish_color_quote 
+      set_color --bold $fish_color_gray 
       echo 'I '
     case replace_one
-      set_color --bold $fish_color_keyword
+      set_color --bold $fish_color_gray
       echo 'R ' 
     case visual
-      set_color --bold $fish_color_redirection
+      set_color --bold $fish_color_gray
       echo 'V '
     case '*'
-      set_color --bold $fish_color_normal 
+      set_color --bold $fish_color_gray 
       echo '? '
   end
   set_color $fish_color_normal
