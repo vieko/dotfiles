2cad530dcc0356fc72095172d852c9e330cfa85c
Vieko Franetovic <48670+vieko@users.noreply.github.com>
2024-04-13 10:52:12 -0600

    improves colors for fish vim mode

--- .config/fish/functions/fish_mode_prompt.fish
+++ .config/fish/functions/fish_mode_prompt.fish
@@ -1,21 +1,21 @@
 function fish_mode_prompt
   switch $fish_bind_mode
     case default
-      set_color --bold blue
+      set_color --bold $fish_color_command
       echo 'N '
     case insert
-      set_color --bold green 
+      set_color --bold $fish_color_quote 
       echo 'I '
     case replace_one
-      set_color --bold red 
+      set_color --bold $fish_color_keyword
       echo 'R ' 
     case visual
-      set_color --bold magenta
+      set_color --bold $fish_color_redirection
       echo 'V '
     case '*'
-      set_color --bold normal 
+      set_color --bold $fish_color_normal 
       echo '? '
   end
-  set_color normal
+  set_color $fish_color_normal
 end
 
